3369aed9e3f7ff86e39baa621ada7b0cb8d8e516
Angela Wong <angela_wong88@yahoo.com>
2018-12-03 21:36:44 +0800

    Day 5 & 6

---
+++ Day 5/home.js
@@ -0,0 +1,77 @@
+var my_name;
+var my_age;
+var my_birthdate;
+
+my_name ="stackup";
+my_age = 23;
+my_birthdate = "20/03/1988";
+
+console.log(my_name);
+console.log(my_age);
+console.log(my_birthdate);
+
+var my_address = "JTC LaunchPad";
+
+var my_test;
+
+console.log(typeof(my_test));
+console.log(typeof(my_name));
+console.log(typeof(my_age));
+
+var firstName;
+var lastName;
+var space;
+
+firstName = "Nick";
+lastName = "Test";
+space = " ";
+
+var fullName = firstName + space + lastName;
+
+var fullName1 = firstName + " " + lastName;
+
+var fullName2 = firstName + 5 + lastName;
+
+console.log(fullName);
+console.log(fullName1);
+console.log(fullName2);
+
+var qty = 2;
+var total = 200;
+var qtyTotal = qty + total;
+
+console.log(qtyTotal);
+
+var firstNumber;
+var secondNumber;
+var totalNumber;
+
+firstNumber = 10;
+secondNumber = 8;
+totalNumber = firstNumber * secondNumber;
+
+console.log(totalNumber);
+
+var compareResult;
+
+compareResult = (firstNumber == secondNumber);
+
+console.log(compareResult);
+
+compareResult = (firstNumber === secondNumber);
+
+console.log(compareResult);
+
+var testA = "Singapore";
+var testB = "Singapore";
+var compareTest = (testA != testB);
+console.log(compareTest);
+
+var myHtmlValue;
+
+myHtmlValue = document.getElementById("my_name").value;
+console.log(myHtmlValue);
+
+
+
+
